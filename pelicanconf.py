360832c2d5d34f8ac230b77b4bf08257ace50e69
rajkirangaddati <rgaddati1@student.gsu.edu>
2016-09-04 23:09:11 -0400

    Added new post

--- pelicanconf.py
+++ pelicanconf.py
@@ -7,7 +7,7 @@ SITENAME = u'Organic Data'
 SITEURL = 'http://rajkirangaddati.com/blog'
 
 PATH = 'content'
-THEME = '/home/rajgad1/blue-penguin'
+THEME = '/home/rajgad1/pelican-clean-blog'
 TIMEZONE = 'US/Eastern'
 OUTPUT_PATH = '/home/rajgad1/rajkirangaddati.com/blog'
 DEFAULT_LANG = u'en'
