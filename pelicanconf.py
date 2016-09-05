9b69f2086b221962bb0a56623145f5253476df85
rajkirangaddati <rgaddati1@student.gsu.edu>
2016-09-04 22:58:22 -0400

    Added theme dir

--- pelicanconf.py
+++ pelicanconf.py
@@ -7,7 +7,7 @@ SITENAME = u'Organic Data'
 SITEURL = 'http://rajkirangaddati.com/blog'
 
 PATH = 'content'
-THEME = 'blue-penguin'
+THEME = '/home/rajgad1/blue-penguin'
 TIMEZONE = 'US/Eastern'
 OUTPUT_PATH = '/home/rajgad1/rajkirangaddati.com/blog'
 DEFAULT_LANG = u'en'
